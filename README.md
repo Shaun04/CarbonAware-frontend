2144aeeec697ea66591096886bf5a404d32660dc
Shaun Mascarenhas <shaunmascarenhas04@gmail.com>
2023-04-06 00:42:10 +0530

    First Public Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Carbon Aware
+Carbon Aware is a browser extension that displays the carbon emissions of websites you visit. With this extension, you can access the amount of carbon dioxide emitted by the website you are currently on. This information can help you make more environmentally conscious decisions when browsing the web.
+
+![My Image](assets/pictures/1.png) ![My Image](assets/pictures/2.png)
+
+## Installation Guide 💾
+   - Prerequisite: Firefox should be installed.
+
+1. Clone this repository or Download.
+
+    ```sh
+    git clone 
+    ```
+
+2. Open Firefox browser
+
+3. Then type the following in the `search bar`
+
+   ```sh
+    about:debugging
+   ```
+
+4. Then click on `This Firefox`
+
+5. Click on `Load Temporary Add-On` and go to the folder in which you have cloned this repo and add the `manifest.txt` file.
+
+
+***THIS WILL ONLY WORK IN A FIREFOX BROWSER SINCE THE MANIFEST FILE IS CONFIGURED FOR IT***
+
+## Contribution
+This extension is currently only compatible with Firefox browsers. However, with some adjustments to the manifest file, it can be made to work on other browsers as well. While efforts to adapt it for use on Chrome are still in the early stages, contributions to this project would be greatly appreciated. Links to resources for getting started with Chrome extensions can be found below. Please feel free to contribute to making this extension work on Chrome and don't forget to submit a pull request with the changes. <br>
+https://developer.chrome.com/docs/extensions/mv3/getstarted/ <br>
+We also welcome bug reports, feature requests, and feedback. Please open an issue in this repository to report a bug or request a feature.
+
+<hr>
+
+__This project is still in its initial stages. Any help will be appreciated.__
---
+++ popup/script.js
@@ -0,0 +1,67 @@
+const button = document.querySelector("button");
+const submit = document.getElementById("submit");
+const urlName = document.querySelector('#content')
+const infoDets = document.querySelector('#info')
+const caution = document.querySelector('#caution')
+const emissionData = document.querySelector('#emission')
+
+function url_formatter(url_raw){
+    let url_split = url_raw.split("/");
+    let newArr = url_split.filter((a) => a);
+    let url_formatted = newArr[1];
+    if (url_formatted == undefined){
+        url_formatted = newArr[0];
+    }
+    console.log(url_formatted);
+    apiCall(url_formatted);
+}
+
+function apiCall(url){
+    urlToApi = "https://vast-rose-agouti-hem.cyclic.app/" + url;
+    console.log(urlToApi);
+    fetch(urlToApi)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            const urlNameFront = '<h3>' + data['0'].url + '</h3>'
+            urlName.insertAdjacentHTML("beforeend", urlNameFront)
+            const deets = '<h3> Estimated CO2 produced per <br> page view is </h3>'
+            infoDets.insertAdjacentHTML('beforeend', deets)
+            const dataFrontEmis = '<h3>' + data['0'].emis + ' grams </h3>'
+            emissionData.insertAdjacentHTML("beforeend", dataFrontEmis)
+        })
+        .catch(error => console.log("Error"));
+}
+
+// button.addEventListener("click", function fun() {
+//     const url = document.getElementById("search").value;
+//     apiCall(url); //calls the function inwhich when you search the url it shows
+// })
+
+// function below listens to the open tabs
+submit.addEventListener("click", function checkThis() {
+    document.getElementById("submit").disabled = true; 
+    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+        let tab = tabs[0];
+        // console.log(url_formatter(tab.url));
+        url_formatter(tab.url);
+    }, console.error)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
